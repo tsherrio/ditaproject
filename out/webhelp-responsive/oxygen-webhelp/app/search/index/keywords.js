e658bf379093b2aac43e2c58281ec5c59df84b26
Tara Sherry <tara.sherry@mycit.ie>
2019-11-13 23:44:56 +0000

    Diff Mark Lang table

--- out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -1,4 +1,4 @@
-define(function() {var keywords=[{w:"Document",p:["p0"]},{w:"Type",p:["p0"]},{w:"Definition",p:["p0"]},{w:"(DTD)",p:["p0"]},{w:"What",p:["p1","p3","p4","p5"]},{w:"is",p:["p1","p3","p4","p5"]},{w:"a",p:["p1","p3"]},{w:"markup",p:["p1","p6"]},{w:"language",p:["p1","p3"]},{w:"Oxegen",p:["p2"]},{w:"XML",p:["p2","p5"]},{w:"Editor",p:["p2"]},{w:"schema",p:["p3","p8"]},{w:"HTML",p:["p4","p7"]},{w:"Different",p:["p6"]},{w:"languages",p:["p6","p8"]},{w:"Some",p:["p7"]},{w:"elements",p:["p7"]},{w:"Creating",p:["p8"]}];
+define(function() {var keywords=[{w:"Document",p:["p0"]},{w:"Type",p:["p0"]},{w:"Definition",p:["p0"]},{w:"(DTD)",p:["p0"]},{w:"What",p:["p1","p3","p4","p5"]},{w:"is",p:["p1","p3","p4","p5"]},{w:"a",p:["p1","p3"]},{w:"markup",p:["p1","p6"]},{w:"language",p:["p1","p3"]},{w:"Oxegen",p:["p2"]},{w:"XML",p:["p2","p5","p6"]},{w:"Editor",p:["p2"]},{w:"schema",p:["p3","p8"]},{w:"HTML",p:["p4","p7"]},{w:"Different",p:["p6"]},{w:"languages",p:["p6","p8"]},{w:"Some",p:["p7"]},{w:"elements",p:["p7"]},{w:"Creating",p:["p8"]}];
 var ph={};
 ph["p0"]=[0, 1, 2, 3];
 ph["p1"]=[4, 5, 6, 7, 8];
@@ -6,7 +6,7 @@ ph["p2"]=[9, 10, 11];
 ph["p3"]=[4, 5, 6, 12, 8];
 ph["p4"]=[4, 5, 13];
 ph["p5"]=[4, 5, 10];
-ph["p6"]=[14, 7, 15];
+ph["p6"]=[14, 10, 7, 15];
 ph["p7"]=[16, 13, 17];
 ph["p8"]=[18, 12, 15];
      return {
